423311c0e3db523f5d2bdfb72419ccbe7cbc6b9c
Marcos Willian <markusuuuu@gmail.com>
2022-11-23 22:34:20 -0300

    chore: created GroupCard component

--- igteams/src/screens/Groups/index.tsx
+++ igteams/src/screens/Groups/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { GroupCard } from '@components/GroupCard';
 
 import { Container } from './styles';
 
@@ -9,6 +10,8 @@ export function Groups() {
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   );
 }
