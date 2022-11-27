4e6bb876137065763816c4bdd8a4e0fc2712efff
Marcos Willian <markusuuuu@gmail.com>
2022-11-27 20:30:06 -0300

    chore: App adjust

--- igteams/App.tsx
+++ igteams/App.tsx
@@ -1,12 +1,12 @@
+import 'react-native-gesture-handler';
+
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 import theme from './src/theme';
 
-import { Groups } from '@screens/Groups';
-import { NewGroup } from '@screens/NewGroup';
-import { Players } from '@screens/Players';
+import { Routes } from './src/routes';
 
 import { Loading } from '@components/Loading';
 
@@ -17,7 +17,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="light-content" translucent />
 
-      {fontsLoaded ? <Players /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
