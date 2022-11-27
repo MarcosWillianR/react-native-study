0b8fdd951052cdfc437e65e91f24fd61231f8d9c
Marcos Willian <markusuuuu@gmail.com>
2022-11-27 20:28:39 -0300

    feat: added modal routing animation in iOS/android

--- igteams/src/routes/app.routes.tsx
+++ igteams/src/routes/app.routes.tsx
@@ -1,14 +1,24 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  createStackNavigator,
+  TransitionPresets,
+} from '@react-navigation/stack';
 
 import { Groups } from '@screens/Groups';
 import { NewGroup } from '@screens/NewGroup';
 import { Players } from '@screens/Players';
 
-const { Navigator, Screen } = createNativeStackNavigator();
+const { Navigator, Screen } = createStackNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        gestureEnabled: true,
+        cardOverlayEnabled: true,
+        ...TransitionPresets.ModalPresentationIOS,
+      }}
+    >
       <Screen name="groups" component={Groups} />
       <Screen name="newGroup" component={NewGroup} />
       <Screen name="players" component={Players} />
