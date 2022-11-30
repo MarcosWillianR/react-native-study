07382ae20b97f4366c2345d4ae889773d711c2e5
Marcos Willian <markusuuuu@gmail.com>
2022-11-29 22:31:43 -0300

    feat: using player storage functions on Player screen

--- igteams/src/screens/Players/index.tsx
+++ igteams/src/screens/Players/index.tsx
@@ -1,6 +1,6 @@
-import { useState, useCallback } from 'react';
-import { FlatList } from 'react-native';
-import { useRoute } from '@react-navigation/native';
+import { useState, useCallback, useMemo } from 'react';
+import { Alert, FlatList } from 'react-native';
+import { useFocusEffect, useRoute } from '@react-navigation/native';
 
 import { Input } from '@components/Input';
 import { Filter } from '@components/Filter';
@@ -10,6 +10,13 @@ import { ListEmpty } from '@components/ListEmpty';
 import { Highlight } from '@components/Highlight';
 import { ButtonIcon } from '@components/ButtonIcon';
 import { PlayerCard } from '@components/PlayerCard';
+
+import { AppError } from '@utils/AppError';
+
+import { playerAddByGroup } from '@storage/player/playerAddByGroup';
+import { PlayerStorageDTO } from '@storage/player/PlayerStorageDTO';
+import { playersGetByGroup } from '@storage/player/playersGetByGroup';
+import { playerRemoveByGroup } from '@storage/player/playerRemoveByGroup';
 
 import { Container, Form, HeaderList, TotalPlayers } from './styles';
 
@@ -21,12 +28,51 @@ export function Players() {
   const route = useRoute();
   const { group } = route.params as RouteParams;
 
-  const [activeTeam, setActiveTeam] = useState('');
-  const [players, setPlayers] = useState([]);
+  const [activeTeam, setActiveTeam] = useState('Time A');
+  const [players, setPlayers] = useState<PlayerStorageDTO[]>([]);
+  const [newPlayerName, setNewPlayerName] = useState('');
 
-  const handleRemovePlayer = useCallback((playerName: string) => {
-    setPlayers(state => state.filter(player => player !== playerName))
-  }, []);
+  const handleGetPlayersByGroup = useCallback(async () => {
+    try {
+      const data = await playersGetByGroup(group);
+      setPlayers(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [group]);
+
+  const handleRemovePlayer = useCallback(async (playerName: string) => {
+    const newPlayer: PlayerStorageDTO = { name: playerName, team: activeTeam }
+    await playerRemoveByGroup(newPlayer, group);
+    await handleGetPlayersByGroup();
+  }, [activeTeam, handleGetPlayersByGroup]);
+
+  const handleAddPlayer = useCallback(async () => {
+    try {
+      if (!newPlayerName.trim().length) {
+        return Alert.alert('Novo Jogador', 'Informe o nome do jogador.');
+      }
+      const newPlayer: PlayerStorageDTO = { name: newPlayerName, team: activeTeam }
+      await playerAddByGroup(newPlayer, group)
+      await handleGetPlayersByGroup();
+      setNewPlayerName('');
+    } catch (error) {
+      if (error instanceof AppError) {
+        Alert.alert('Novo Jogador', error.message)
+      } else {
+        Alert.alert('Novo Jogador', 'Não foi possível criar um novo jogador');
+        console.log(error);
+      }
+    }
+  }, [newPlayerName, activeTeam, group]);
+
+  const filteredPlayersByGroup = useMemo(() => {
+    return players.filter(player => player.team === activeTeam);
+  }, [players, activeTeam]);
+
+  useFocusEffect(useCallback(() => {
+    handleGetPlayersByGroup();
+  }, []));
 
   return (
     <Container>
@@ -38,9 +84,14 @@ export function Players() {
       />
 
       <Form>
-        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+        <Input
+          placeholder="Nome da pessoa"
+          autoCorrect={false}
+          value={newPlayerName}
+          onChangeText={setNewPlayerName}
+        />
 
-        <ButtonIcon icon="add" />
+        <ButtonIcon icon="add" onPress={handleAddPlayer} />
       </Form>
 
       <HeaderList>
@@ -56,18 +107,18 @@ export function Players() {
             />
           )}
         />
-        <TotalPlayers>{players.length}</TotalPlayers>
+        <TotalPlayers>{filteredPlayersByGroup.length}</TotalPlayers>
       </HeaderList>
 
       <FlatList
-        data={players}
-        keyExtractor={item => item}
-        renderItem={({ item }) => <PlayerCard name={item} onRemove={handleRemovePlayer} />}
+        data={filteredPlayersByGroup}
+        keyExtractor={item => item.name}
+        renderItem={({ item }) => <PlayerCard name={item.name} onRemove={handleRemovePlayer} />}
         ListEmptyComponent={() => <ListEmpty message="Não há pessoas nesse time" />}
         showsVerticalScrollIndicator={false}
         contentContainerStyle={[
           { paddingBottom: 100 },
-          players.length === 0 && { flex: 1 }
+          filteredPlayersByGroup.length === 0 && { flex: 1 }
         ]}
       />
 
