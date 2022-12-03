002cd903443fb2f34b468275462de394f99df92a
Marcos Willian <markusuuuu@gmail.com>
2022-12-03 13:17:50 -0300

    chore: creating Loading component

--- ignitegym/App.tsx
+++ ignitegym/App.tsx
@@ -2,14 +2,21 @@ import { View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from "native-base";
 
+import { Loading } from '@components/Loading';
+
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <NativeBaseProvider>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
 
-      {fontsLoaded && <View />}
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
