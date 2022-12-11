d218f8a092287f41df9172db2c8d4392d6c36af7
Marcos Willian <markusuuuu@gmail.com>
2022-12-11 18:06:22 -0300

    chore: finish UI

--- todolist/src/screens/Home.tsx
+++ todolist/src/screens/Home.tsx
@@ -1,13 +1,17 @@
-import { Heading, HStack, Image, Input, VStack, Icon, Pressable } from "native-base";
+import { useState } from 'react';
+import { HStack, Image, Input, VStack, Icon, Button, Text, Box, FlatList } from "native-base";
 import { Feather } from '@expo/vector-icons';
 
 import logoPng from '@assets/Logo.png';
+import ClipboardPng from '@assets/Clipboard.png';
 
 export function Home() {
+  const [tasks, setTasks] = useState<string[]>([]);
+
   return (
     <VStack bg="gray.600" flex={1}>
-      <VStack bg="gray.700" pt={16} px={8} pb={6} alignItems="center">
-        <Image source={logoPng} />
+      <VStack bg="gray.700" mb={6} pt={16} px={6} pb={6} alignItems="center">
+        <Image source={logoPng} alt="Todo" />
 
         <HStack top={12}>
           <Input
@@ -27,11 +31,44 @@ export function Home() {
             }}
           />
 
-          <Pressable bg="secondary.700" p={5} rounded="md" ml={1}>
+          <Button bg="secondary.700" p={5} rounded="md" ml={1} _pressed={{ bg: 'secondary.500' }}>
             <Icon as={Feather} name="plus-circle" w={16} h={16} color="white" />
-          </Pressable>
+          </Button>
         </HStack>
       </VStack>
+
+      <HStack mt={8} mx={6} pb={5} justifyContent="space-between" borderBottomWidth={1} borderBottomColor="gray.500">
+        <HStack alignItems="center">
+          <Text fontSize={14} color="secondary.500" fontFamily="heading">Criadas</Text>
+
+          <Box bg="gray.500" px={2} rounded="lg" ml={2}>
+            <Text color="white">2</Text>
+          </Box>
+        </HStack>
+
+        <HStack alignItems="center">
+          <Text fontSize={14} color="primary.500" fontFamily="heading">Concluidas</Text>
+
+          <Box bg="gray.500" px={2} rounded="lg" ml={2}>
+            <Text color="white">4</Text>
+          </Box>
+        </HStack>
+      </HStack>
+
+      <FlatList
+        data={tasks}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        ListEmptyComponent={() => (
+          <VStack alignItems="center" justifyContent="center" mt={12}>
+            <Image source={ClipboardPng} alt="Todo" />
+            <Text color="gray.300" fontSize={14} mt={4}>
+              <Text fontFamily="heading">Você ainda não tem tarefas cadastradas</Text>{`\n`}
+              Crie tarefas e organize seus itens a fazer
+            </Text>
+          </VStack>
+        )}
+      />
     </VStack>
   )
 }
