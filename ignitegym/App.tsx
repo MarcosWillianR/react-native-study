328907993992a8ac2f264e968319211fc0aa7204
Marcos Willian <markusuuuu@gmail.com>
2022-12-12 23:52:23 -0300

    chore: finish ImagePicker feature

--- ignitegym/App.tsx
+++ ignitegym/App.tsx
@@ -1,23 +1,21 @@
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { NativeBaseProvider } from "native-base";
 
+import { THEME } from './src/theme';
+
 import { Loading } from '@components/Loading';
 
-import { THEME } from './src/theme';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
-      {fontsLoaded ? <View /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
