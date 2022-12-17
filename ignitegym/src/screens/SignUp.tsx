612a79dddcc6357971ffaf2ff08634dbccc41bee
Marcos Willian <markusuuuu@gmail.com>
2022-12-16 22:34:06 -0300

    chore: added React Hook Form validation on SignUp

--- ignitegym/src/screens/SignUp.tsx
+++ ignitegym/src/screens/SignUp.tsx
@@ -1,7 +1,9 @@
 import { useCallback } from 'react';
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base';
 import { useNavigation } from '@react-navigation/native';
-import { useForm, Controller, UseFormHandleSubmit } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup'
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -16,9 +18,29 @@ type FormDataProps = {
   password_confirm: string;
 }
 
+const signUpSchema = yup.object({
+  name: yup
+    .string()
+    .required('Informe o nome'),
+  email: yup
+    .string()
+    .required('Informe o e-mail')
+    .email('E-mail inválido'),
+  password: yup
+    .string()
+    .required('Informe a senha')
+    .min(6, 'A senha deve ter pelo menos 6 dígitos'),
+  password_confirm: yup
+    .string()
+    .required('Confirme a senha')
+    .oneOf([yup.ref('password'), null], 'A confirmação da senha não confere')
+})
+
 export function SignUp() {
   const navigation = useNavigation();
-  const { control, handleSubmit } = useForm<FormDataProps>();
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  });
 
   const handleGoBack = useCallback(() => {
     navigation.goBack();
@@ -60,7 +82,12 @@ export function SignUp() {
             control={control}
             name="name"
             render={({ field: { onChange, value } }) => (
-              <Input placeholder="Nome" onChangeText={onChange} value={value} />
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
             )}
           />
 
@@ -74,6 +101,7 @@ export function SignUp() {
                 autoCapitalize="none"
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.email?.message}
               />
             )}
           />
@@ -87,6 +115,7 @@ export function SignUp() {
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.password?.message}
               />
             )}
           />
@@ -101,6 +130,7 @@ export function SignUp() {
                 secureTextEntry
                 onChangeText={onChange}
                 value={value}
+                errorMessage={errors.password_confirm?.message}
               />
             )}
           />
